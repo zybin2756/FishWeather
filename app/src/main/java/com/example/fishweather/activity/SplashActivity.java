7e708a126fd10f6c278273da436113fd3f97901e
zyb <zybns@qq.com>
2017-03-20 23:38:37 +0800

    add menu

--- app/src/main/java/com/example/fishweather/activity/SplashActivity.java
+++ app/src/main/java/com/example/fishweather/activity/SplashActivity.java
@@ -5,22 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
-
-import com.example.fishweather.FishApplication;
 import com.example.fishweather.R;
 import com.example.fishweather.db.City;
 import com.example.fishweather.util.Constants;
 import com.example.fishweather.util.HttpUtil;
 import com.example.fishweather.util.ParseUtil;
-import com.squareup.leakcanary.RefWatcher;
-
 import org.litepal.crud.DataSupport;
-
-import java.util.List;
 
 /**
  * Created by Administrator on 2017/3/19.
@@ -37,6 +30,7 @@ public class SplashActivity extends Activity {
         progressBar = (RelativeLayout) findViewById(R.id.progressBar);
         City city = DataSupport.findFirst(City.class);
         if(city == null){
+            progressBar.setVisibility(View.VISIBLE);
             String path = "http://files.heweather.com/china-city-list.json";
             HttpUtil.sendOkHttpRequest(path,new HttpUtil.HttpCallBack(){
                 @Override
@@ -67,11 +61,13 @@ public class SplashActivity extends Activity {
             switch (msg.what){
                 case Constants.LOAD_CITY_INFO_ERROR:
                     Toast.makeText(SplashActivity.this,"加载数据失败!",Toast.LENGTH_SHORT);
+                    finish();
                     break;
                 case Constants.LOAD_CITY_INFO_FINISH:
                     progressBar.setVisibility(View.GONE);
                     Intent intent = new Intent(SplashActivity.this,WeatherInfosActivity.class);
                     startActivity(intent);
+                    finish();
                     break;
             }
         }
--- app/src/main/java/com/example/fishweather/activity/WeatherInfosActivity.java
+++ app/src/main/java/com/example/fishweather/activity/WeatherInfosActivity.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.fishweather.R;
+
+import java.util.zip.Inflater;
 
 /**
  * Created by Administrator on 2017/3/20 0020.
@@ -16,7 +20,27 @@ public class WeatherInfosActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_changeCity:
+                break;
+            case R.id.action_setting:
+                break;
+            case R.id.action_exit:
+                finish();
+                break;
+        }
+        return true;
     }
 }
